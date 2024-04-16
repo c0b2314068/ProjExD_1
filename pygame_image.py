21be869790083cb47194102986eb65688ca9a78a
c0b2314068 <c0b2314068.edu.teu.ac.jp>
2024-04-16 15:45:26 +0900

    スクロール修正

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,8 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    org_bg_img = pg.image.load("fig/pg_bg.jpg")
+    flp_bg_img = pg.transform.flip(org_bg_img, 1, 0)
     pl_img = pg.transform.flip(pg.image.load("fig/3.png"), 1, 0)
     bg_x = 0
     tmr = 0
@@ -17,11 +18,14 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-bg_x, 0])
+        screen.blit(org_bg_img, [-bg_x, 0])
+        screen.blit(flp_bg_img, [-bg_x+1600, 0])
+        screen.blit(org_bg_img, [-bg_x+3200, 0])
+        screen.blit(flp_bg_img, [-bg_x+4800, 0])
         screen.blit(pl_img, [300, 200])
         pg.display.update()
         bg_x += 1
-        bg_x %= 800
+        bg_x %= 3200
         tmr += 1        
         clock.tick(200)
 
