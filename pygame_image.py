b0b524dbcb510b92bcc6af89fd2617ec907e0c0a
c0b2314068 <c0b2314068.edu.teu.ac.jp>
2024-04-16 15:39:02 +0900

    背景スクロール

--- pygame_image.py
+++ pygame_image.py
@@ -11,14 +11,17 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     pl_img = pg.transform.flip(pg.image.load("fig/3.png"), 1, 0)
+    bg_x = 0
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-bg_x, 0])
         screen.blit(pl_img, [300, 200])
         pg.display.update()
+        bg_x += 1
+        bg_x %= 800
         tmr += 1        
         clock.tick(200)
 
