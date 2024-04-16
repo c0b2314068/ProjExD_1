98a66ad1a08cbc99cbefa58d3e7a98414201b41d
c0b2314068 <c0b2314068.edu.teu.ac.jp>
2024-04-16 15:35:18 +0900

    FPS変更

--- pygame_image.py
+++ pygame_image.py
@@ -20,7 +20,7 @@ def main():
         screen.blit(pl_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
